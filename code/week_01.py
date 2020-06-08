206deb095cc4faf109f4269781272a787c5d739b
Minji Song <ssong758@gmail.com>
2020-06-08 10:13:42 +0900

    upload week01 & 02 answers

---
+++ code/week_01.py
@@ -0,0 +1,46 @@
+
+from collections import deque 
+
+def solution(board, moves):
+    moves = deque(moves)
+    buckets = []
+    cnt = 0 
+
+    while moves:
+        move = moves.popleft()
+
+        for i in range(len(board)):
+            doll = board[i][move-1]
+
+            if doll != 0:
+                board[i][move-1] = 0
+
+                if buckets and buckets[-1] == doll:
+                    buckets.pop()
+                    cnt += 2
+                else:
+                    buckets.append(doll)
+                break
+
+    return cnt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
